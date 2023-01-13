a3f3cea7de72b759ce21f2bd775e5cde3756fec4
bryansomto <bryansomto@gmail.com>
2023-01-13 03:36:13 +0100

    extra modif

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT) BEGIN
 DECLARE w_avg_score FLOAT;
 SET @w_avg_score = (
-        SELECT SUM(score * weight) / SUM(weight)
+        SELECT AVG(score)
         FROM users AS U
             JOIN corrections as C ON U.id = C.user_id
             JOIN projects AS P ON C.project_id = P.id
