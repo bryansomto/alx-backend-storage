8d4fffeb22eb17f7f4c8e0e9db959fcf51f91475
bryansomto <bryansomto@gmail.com>
2023-01-13 03:32:22 +0100

    extra modif

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -2,7 +2,7 @@
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT) BEGIN
 DECLARE w_avg_score FLOAT;
-SET w_avg_score = (
+SET @w_avg_score = (
         SELECT SUM(score * weight) / SUM(weight)
         FROM users AS U
             JOIN corrections as C ON U.id = C.user_id
--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,6 +1,7 @@
--- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and stores the average weighted score for all students
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-DELIMITER $$ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN
+DELIMITER | CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN
 UPDATE users AS U,
     (
         SELECT U.id,
@@ -12,4 +13,4 @@ UPDATE users AS U,
     ) AS WA
 SET U.average_score = WA.w_avg
 WHERE U.id = WA.id;
-END $$ DELIMITER;
+END;
