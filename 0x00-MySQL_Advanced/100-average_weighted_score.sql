8a91fdc263af47dd856900fb661b6ff19ebc9f2b
bryansomto <bryansomto@gmail.com>
2023-01-13 03:35:08 +0100

    extra modif

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,6 +1,6 @@
 -- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER $$ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT) BEGIN
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT) DELIMITER $$ BEGIN
 DECLARE w_avg_score FLOAT;
 SET @w_avg_score = (
         SELECT SUM(score * weight) / SUM(weight)
